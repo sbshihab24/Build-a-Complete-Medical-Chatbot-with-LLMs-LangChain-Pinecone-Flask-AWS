6b7c619461dd9b36f62a17a1dae6d8cdaead571c
Mehedi Hasan Shihab <sbshihab2000@gmail.com>
2026-01-13 05:45:00 +0600

    Update requirements, setup, and add research notebooks

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages , setup
+
+setup(
+    name="medical_chatbot",
+    version="1.0.0",
+    author="Mehedi Hasan Shihab",
+    author_email="sbshihab2000@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
