7febd4622531d73d9b7b76a4724f177ff1ec7abb
Mehedi Hasan Shihab <sbshihab2000@gmail.com>
2026-01-12 06:12:42 +0600

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# creating Directory
+
+mkdir -p src
+mkdir -p resources
+
+# creating files
+touch src/main.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch README.md
+touch app.py
+touch requirements.txt
+touch research/trials.py
+
+
+echo directory and files created seccessfully
